9b516a6bdb3d8eec60be542b520a48e8c156432d
Steve <steveboris>
2020-03-26 21:03:52 +0100

    db modified

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=yii2crash',
-    'username' => 'steve',
-    'password' => 'sudosuapt-get1',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
